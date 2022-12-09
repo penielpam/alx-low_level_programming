3007d912fa0f05006e2d2b3115ba71126deae3ac
Peniel <penielpam@gmail.com>
2022-12-08 19:24:15 -0800

    More changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  *   main - Entry
  *     Return: Always 0
- *       */
+ */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
+printf("with proper grammar,but the outcome is a piece of art,\n");
 	return (0);
 }
