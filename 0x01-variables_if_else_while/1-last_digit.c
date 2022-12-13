0a23f533dad6bc319dc0ad43d3d3e31878897a65
Peniel <penielpam@gmail.com>
2022-12-12 18:25:54 -0800

    Made changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,22 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-/**
-*
-* main - Entry point
-*
-* Return: Always 0 (success)
-*/
-int main(void)
+#include <time.h> 
+#include <stdio.h> 
+/. more headers goes there */ 
+/••  main - Entry point
+ • Return: Always 0 (Success) 
+ int main(void)
 {
-	int n;
-	int m;
-
+	int n, lastNum;
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	m = n % 10;
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	if (m == 0)
-		printf("Last digit of %d is %d and is 5\n", n, m);
-	if (m < 6 && m != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m); 
-	return (0);
+	n = and() - RAND_MAX / 2;
+	lastNum = n % 10; 
+	if (lastNum > 5)
+		printi('iast digit of %4 is %d and is greater than 5\n", n, lastNum);
+	else ii (14,1Num == 0)
+		printl("Last digit of %d is Xd and is 0\n", n, lastNum);
+	else
+		printf("tast digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+	return (0); 
 }
+
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while (d <= 'z')
+	{
+		putchar(c);
+			c++;
+	}
+	putchar('\n');
+	return (0);
+
+}
