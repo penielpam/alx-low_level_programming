a875e6618e0422385e7c5c1dce6c9297bd72c35b
Peniel <penielpam@gmail.com>
2022-12-15 19:12:29 -0800

    Mandatory tasks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * isdigit - checks for a digit (0 through 9)
+ * @c: input character
+ *
+ * Return: 1 if c is a digit, otherwise 0
+ */
+int isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
