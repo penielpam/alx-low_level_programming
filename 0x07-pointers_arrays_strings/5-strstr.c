389b8e11086a3672bb9d21e09d5243628f474ad5
Peniel <penielpam@gmail.com>
2023-01-02 13:08:46 -0800

    Mandatory tasks

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,20 +1,32 @@
 #include "main.h"
+
 /**
- * string_toupper - change lowercase to uppercase
- * @s:string
- * Return:char
+ * _strstr - locates a substring
+ * @haystack: the longer string to search
+ * @needle: the first occurrence of the substring
+ * Return: a pointer beg of substring or @Null if it not found.
  */
-char *string_toupper(char *s)
+
+char *_strstr(char *haystack, char *needle)
 {
+	unsigned int i = 0, j = 0;
 
-	int i;
-
-	i = 0;
-	while (*(s + i))
+	while (haystack[i])
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
-		i++;
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
-	return (s);
+	return (0);
 }
